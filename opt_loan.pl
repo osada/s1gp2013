ee0b5a0607128d6c47587216ca5aa60123fc27a8
Shigeyuki Osada <osada@osada.info>
2013-03-10 11:51:25 +0900

    Considering periods

--- opt_loan.pl
+++ opt_loan.pl
@@ -1,13 +1,14 @@
 #!/opt/local/bin/perl
 
-#STDIN:    Needs List    
+#STDIN:    Needs List
 #$ARGV[0]: Fund
 #$ARGV[1]: Trial Times
 #$ARGV[2]: Seed
 
 #Definition
-$MONTH_START = 201401;
-$MONTH_END   = 201412;
+$MONTH_START   = 201401;
+$MONTH_END     = 201412;
+$ATTEMPT_LIMIT = 100;
 
 if ($#ARGV != 2) {
     print "Error: missing argment\n";
@@ -23,83 +24,99 @@ $SEED  = $ARGV[2];
 srand $SEED;
 
 # Initialize Output Variables
-$USED_MAX  = -1;
-$GAIN_MAX  = -1;
-@LOAN_LIST_MAX = ();
+$GAIN_MAX            = -1;
+$USED_RATE_MAX       = 0;
+@LOAN_LIST_MAX       = ();
+$STAT_CANCEL_BY_DUP  = 0;
+$STAT_CANCEL_BY_OVER = 0;
+$STAT_UPDATE         = 0;
 
 # Read Data
 @NEED_LIST = <STDIN>;
 
 foreach (1 .. $TRIAL) {
     # Initialization
-    $USED = 0;
-    $GAIN = 0;
-    $MONTH_NOW = $MONTH_START;
+	$FUND      = $ARGV[0];
+    $USED      = 0;
+	$USED_RATE = 0;
+    $GAIN      = 0;
     @LOAN_LIST = ();
 
-    LOOPMARK: while ($USED < $FUND) {
-	# Select one data
-	$ELEM = int(rand($#NEED_LIST + 1));
-	$NEEDER = $NEED_LIST[$ELEM];
-	
-	# Cleansing data
-	chomp($NEEDER);
-	@NEEDER_INFO = split(/\s+/, $NEEDER);
-	$ID   = $NEEDER_INFO[0];
-	$NEED = $NEEDER_INFO[1]; 
-	$RATE = $NEEDER_INFO[2];
-	$RISK = $NEEDER_INFO[3];
-    
-	# check duplicated loan
-	foreach (@LOAN_LIST) {
-	    if ($ID == $_) {
-		next LOOPMARK;
-	    }
+	#print "--- Traial: $_ ---\n";
+
+	for ($MONTH_NOW = $MONTH_START; $MONTH_NOW <= $MONTH_END; $MONTH_NOW++) {
+		#print "MONTH: $MONTH_NOW, (FUND, USED) = ($FUND, $USED)\n";
+		# Collecting loans
+		foreach $CUSTOMER (@LOAN_LIST) {
+			chomp($CUSTOMER);
+			@CUSTOMER_INFO = split(/\s+/, $CUSTOMER);
+			$ID     = $CUSTOMER_INFO[0];
+			$NEED   = $CUSTOMER_INFO[1];
+			$RATE   = $CUSTOMER_INFO[2];
+			$MONTH  = $CUSTOMER_INFO[3];
+			$PERIOD = $CUSTOMER_INFO[4];
+			
+			if ($MONTH_NOW == $MONTH + $PERIOD) {
+				$FUND = $FUND + $NEED * $RATE * $PERIOD / 12.0;
+				$USED = $USED - $NEED;
+			}
+		}
+		#print "clearing -> ($FUND, $USED)\n";
+		# Loaning
+	  LOOPMARK: for ($ATTEMPT = 0; $USED < $FUND && $ATTEMPT < $ATTEMPT_LIMIT;
+					 $ATTEMPT++) {
+		  # Select one data
+		  $ELEM = int(rand($#NEED_LIST + 1));
+		  $NEEDER = $NEED_LIST[$ELEM];
+		  
+		  # Cleansing data
+		  chomp($NEEDER);
+		  @NEEDER_INFO = split(/\s+/, $NEEDER);
+		  $ID     = $NEEDER_INFO[0];
+		  $NEED   = $NEEDER_INFO[1];
+		  $RATE   = $NEEDER_INFO[2];
+		  $MONTH  = $NEEDER_INFO[3];
+		  $PERIOD = $NEEDER_INFO[4]; 
+		  
+		  # check duplicated loan
+		  foreach (@LOAN_LIST) {
+			  if ($ID == $_) {
+				  $STAT_CANCEL_BY_DUP++;
+				  next LOOPMARK;
+			  }
+		  }
+		  
+		  # drop a candidate having not $MONTH_NOW
+		  if ($MONTH != $MONTH_NOW) {next LOOPMARK;}
+		  
+		  # drop a candidate over period
+		  if ($MONTH_END < $MONTH + $PERIOD) {next LOOPMARK;}
+
+		  # add loan list if possible
+		  if ($USED + $NEED <= $FUND) {
+			  push(@LOAN_LIST, $NEEDER);
+			  $USED = $USED + $NEED;
+			  $GAIN = $GAIN + $NEED * $RATE * $PERIOD / 12.0;
+		  }
+		  else {
+			  last LOOPMARK;
+		  }
+	  }
+		$USED_RATE = $USED_RATE + $USED / $FUND / 12.0;
+		#print "loaning -> ($FUND, $USED)\n";
+		#print "GAIN: $GAIN, USED_RATE: " . $USED / $FUND ."\n\n"
+
 	}
 
-	# drop a candidate with higher risk than interest rate
-	if ($RATE < $RISK) {
-	    next LOOPMARK;
-	}
-
-	# add loan list if possible
-	if ($USED + $NEED <= $FUND) {
-	    push(@LOAN_LIST, $ID);
-	    $USED = $USED + $NEED;
-	    $GAIN = $GAIN + $NEED * $RATE;
-	    $LOST = $LOST + $NEED * $RISK;
-	}
-	else {
-	    last LOOPMARK;
-	}
-    }
-
-    $SUM = $GAIN - $LOST;
-
-    if ($SUM > $SUM_MAX) {
-	$SUM_MAX  = $SUM;
-	$GAIN_MAX = $GAIN;
-	$LOST_MIN = $LOST;
-	$USED_MAX = $USED;
-	@LOAN_LIST_MAX = @LOAN_LIST
-    }
+	if ($GAIN > $GAIN_MAX) {
+			$GAIN_MAX = $GAIN;
+			$USED_RATE_MAX = $USED_RATE;
+			@LOAN_LIST_MAX = @LOAN_LIST;
+		}
 }
 
 # Output Results
-print "SUM_MAX = $SUM_MAX (GAIN_MAX = $GAIN_MAX, LOST_MIN = $LOST_MIN\n";
-print "USED_MAX / FUND = $USED_MAX / $FUND (". $USED_MAX/$FUND .")\n";
-foreach $LOAN_ID (@LOAN_LIST_MAX) {
-    foreach $NEEDER (@NEED_LIST) {
-	chomp($NEEDER);
-	@NEEDER_INFO = split(/\s+/, $NEEDER);
-	if ($LOAN_ID == $NEEDER_INFO[0]) {
-	    print "$NEEDER\n";
-	    last;
-	}
-    }
+print "GAIN_MAX = $GAIN_MAX, USED_RATE_MAX = $USED_RATE_MAX\n";
+foreach (@LOAN_LIST_MAX) {
+	print "$_\n";
 }
-
-#print "$ID\t$NEED\t$RATE\t$RISK\n";
-
-
-
