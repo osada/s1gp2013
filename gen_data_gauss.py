b507a8aa77f2313888afb3b2aa7aa0983b529b4a
Shigeyuki Osada <osada@osada.info>
2013-04-22 17:16:47 +0900

    add rate caluclation

--- gen_data_gauss.py
+++ gen_data_gauss.py
@@ -18,6 +18,7 @@ seed         = int(sys.argv[2])
 
 # Constant Variables
 need_min     = 4000
+need_wm      = 30000     # Watermark for calculating rate
 month_offset = 201401
 period_min   = 3
 period_max   = 24
@@ -49,16 +50,16 @@ def get_need(need_min):
     return need
 
 def get_rate(need):
-    rate = 0
-    # select rate concidered from need
-    while rate < rate_min || rate > rate_max
+    rate = 0.0
+    while rate < rate_min or rate > rate_max:
         rv = random.uniform(0, 1)
-        if rv < 0.5:
-            need = int(norm.rvs(loc=800, scale=400)) * 10
-        elif rv < 0.65:
-            need = int(norm.rvs(loc=1300, scale=100)) * 10
+        if rv < 0.6:
+            rate = int(norm.rvs(loc=45, scale=20)) / 1000.0
+        elif rv < 0.7:
+            rate = int(norm.rvs(loc=150, scale=10)) / 1000.0
         else:
-            need = int(norm.rvs(loc=2000, scale=200)) * 10
+            rate = int(norm.rvs(loc=80, scale=30)) / 1000.0
+        rate = round(rate * (1.0 - 1.0 * need / need_wm), 3) 
     return rate
 
 # Customer ID: 0 -- data_size
@@ -66,7 +67,8 @@ for id in range(data_size):
     # Need Money: 4000 --
     need = get_need(need_min)
     # Interest Rate: 0.0100 -- 0.1920
-    rate = round(random.uniform(rate_min, rate_max), 4)
+    # rate = round(random.uniform(rate_min, rate_max), 4)
+    rate = get_rate(need)
     # From Month: 201401 -- 201510
     start_mon  = random.randint(0, period_max - period_min)
     from_mon = month_offset + start_mon % 12 + math.floor(start_mon / 12) * 100
